0aa2040bd3f975875163ea18db762169973bf930
dajiyuanzi <dajiyuanzi@foxmail.com>
2019-08-20 16:49:45 +0800

    play

--- practice/mis40_musicPlayer/js/index.js
+++ practice/mis40_musicPlayer/js/index.js
@@ -17,16 +17,16 @@ var EventCenter = {
 
 
 
-var Footer = {
+var Footer = {//底部的音乐滑动
   init: function(){
     this.$footer = $('footer')
     this.$ul = this.$footer.find('ul')
     this.$box = this.$footer.find('.box')
     this.$leftBtn = this.$footer.find('.icon-left')
     this.$rightBtn = this.$footer.find('.icon-right')
-    this.isToEnd = false
-    this.isToStart = true
-    this.isAnimate = false
+    this.isToEnd = false //下面的曲目横向列表是否滑到底部，默认 没到底。
+    this.isToStart = true //下面的曲目横向列表是否滑到开头
+    this.isAnimate = false //当一个滑动动画为执行完，此状态为false，防止重复点击。连续点击播放滚动时，会溢出（下面的监听不管用了），所以要设置此状态。
 
     this.bind()
     this.render()
@@ -34,18 +34,20 @@ var Footer = {
 
   bind: function(){
     var _this = this
-    this.$rightBtn.on('click', function(){
-      if(_this.isAnimate) return
+
+    this.$rightBtn.on('click', function(){//朝右可以滚动的距离
+      if(_this.isAnimate) return//如果上个滚动动画还在播放，当前这个点击就不要触发了，毙掉。
 
       var itemWidth = _this.$box.find('li').outerWidth(true)
-      var rowCount = Math.floor(_this.$box.width()/itemWidth)
+      var rowCount = Math.floor(_this.$box.width()/itemWidth)//求底部一共几个音乐
       if(!_this.isToEnd){
-        _this.isAnimate = true
-        _this.$ul.animate({
-          left: '-='+rowCount*itemWidth
-        }, 400, function(){
+        _this.isAnimate = true//当前滑动播放，就设为true。那么之后重复点击的动画就在上面return，不再播放了。
+        _this.$ul.animate({//$(selector).animate(styles,speed,easing,callback)方法执行 CSS 属性集的自定义动画。该方法通过CSS样式将元素从一个状态改变为另一个状态。
+          left: '-='+rowCount*itemWidth //自减相当于ul在容器内朝左移动移动，将右边的溢出内容移到容器内以可见
+        }, 400, function(){//播放完成后
           _this.isAnimate = false
           _this.isToStart = false
+          //当右滚到底部时，即容器的宽度-溢出容器的内容部分 的长度（是负值）>= 内容的全部长度
           if(parseFloat(_this.$box.width()) - parseFloat(_this.$ul.css('left')) >= parseFloat(_this.$ul.css('width')) ){
             _this.isToEnd = true
           }
@@ -53,7 +55,7 @@ var Footer = {
       }
     })
 
-    this.$leftBtn.on('click', function(){
+    this.$leftBtn.on('click', function(){//朝左可以滚动的距离
       if(_this.isAnimate) return
       var itemWidth = _this.$box.find('li').outerWidth(true)
       var rowCount = Math.floor(_this.$box.width()/itemWidth)
@@ -124,7 +126,7 @@ var Footer = {
 
 
 
-var App = {
+var App = {//播放
   init: function(){
     this.channelId = 'public_shiguang_80hou'
     this.channelName = '80鍚�'
@@ -136,21 +138,21 @@ var App = {
     this.collections = this.loadFromLocal()
     this.bind()
 
-    EventCenter.fire('select-albumn', {
+    EventCenter.fire('select-albumn', {//自己触发trigger一些自定义事件
       channelId: '0',
       channelName: '鎴戠殑鏈€鐖�'
     })
   },
   bind: function(){
-    var _this = this
-    EventCenter.on('select-albumn', function(e, channel){
+    var _this = this//下面函数内的this指向的不是app类。
+    EventCenter.on('select-albumn', function(e, channel){//自定义事件，选音乐时，触发。EventCenter是上面自定义的类。
       console.log('select ', channel)
       _this.channelId = channel.channelId
       _this.channelName = channel.channelName
       _this.loadSong()
     })
 
-    this.$container.find('.btn-play').on('click', function(){
+    this.$container.find('.btn-play').on('click', function(){//播放按钮
       if($(this).hasClass('icon-pause')){
         $(this).removeClass('icon-pause').addClass('icon-play')
         _this.audio.pause()
@@ -164,12 +166,12 @@ var App = {
       _this.loadSong()
     })
 
-    this.audio.addEventListener('play', function(){
+    this.audio.addEventListener('play', function(){//play事件 是audio类里自带的
       clearInterval(_this.clock)
       _this.clock = setInterval(function(){
         _this.updateState()
         _this.setLyric()
-      }, 1000)
+      }, 1000)//以免进度变化不均匀，所以设置定时，一秒走一格
       console.log('play')
     })
 
@@ -229,9 +231,10 @@ var App = {
     
     this.loadLyric(song.sid)
   },
-  updateState: function(){
+
+  updateState: function(){//进度条的更新
     var timeStr = Math.floor(this.audio.currentTime/60)+':'
-      + (Math.floor(this.audio.currentTime)%60/100).toFixed(2).substr(2)
+      + (Math.floor(this.audio.currentTime)%60/100).toFixed(2).substr(2) //获得已播放的分秒，toFix(2)规定小数四舍五入为2位，substr()方法可在字符串中抽取从 start 下标开始的指定数目的字符。
     this.$container.find('.current-time').text(timeStr)
     this.$container.find('.bar-progress').css('width', this.audio.currentTime/this.audio.duration * 100 + '%')
   },
@@ -287,7 +290,7 @@ var App = {
 
 
 //http://js.jirengu.com/zuvar/1/edit?html,js,console,output
-$.fn.boomText = function(type){
+$.fn.boomText = function(type){ //jquery自定义方法就是$.fn.func，方便之后调用。
   type = type || 'rollIn'
   console.log(type)
   this.html(function(){
