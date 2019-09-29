d9fd8100628c2c73c3841578c5517b00ad379be9
dajiyuanzi <dajiyuanzi@foxmail.com>
2019-09-29 20:36:55 +0800

    fix

--- practice/vue_demo1/src/main.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import App from './App.vue'
-
-Vue.config.productionTip = false
-
-new Vue({
-  render: h => h(App),
-}).$mount('#app') //看下index中 哪个对应，改一改看
-//CRM(copy抄，run运行，modify改已有的代码)->不会代码，也能用
