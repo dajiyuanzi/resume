2a3db2a9b4c1ee3a9ccce229a09040b55c9fbf63
dajiyuanzi <dajiyuanzi@foxmail.com>
2019-12-05 11:14:25 +0800

    将 SignUpForm 改成一个函数

--- practice/ReactList/todolist_leancloud/src/SignUpForm.js
+++ practice/ReactList/todolist_leancloud/src/SignUpForm.js
@@ -1,37 +1,35 @@
-import React, {Component} from 'react';
+import React from 'react';
 
-export default class SignUpForm extends Component {
-  render(){
-    return (
-      <form className="signUp" onSubmit={this.props.onSubmit.bind(this)}> {/* Register */}
-        <div className="row">
-          <label>Email</label>
-          <input 
-            type="text"
-            value={this.props.formData.email}
-            onChange={this.props.onChange.bind(null, 'email')}
-          />
-        </div>
-        <div className="row">
-          <label>User Name</label>
-          <input 
-            type="text" 
-            value={this.props.formData.username} 
-            onChange={this.props.onChange.bind(null, 'username')} 
-          />{/* bind 不仅可以绑定 this，还可以绑定第一个参数,。这里执行context设为null，因为UserDialog绑定了它的context */}
-        </div>
-        <div className="row">
-          <label>Password</label>
-          <input type="password"
-            value={this.props.formData.password}
-            onChange={this.props.onChange.bind(null, 'password')}
-          />
-        </div>
-        <div className="row actions">
-          <button type="submit">Sign Up</button>
-        </div>
-      </form>
-    )
-  }
+export default function(props) {
+  return (
+    <form className="signUp" onSubmit={this.props.onSubmit.bind(this)}> {/* Register */}
+      <div className="row">
+        <label>Email</label>
+        <input 
+          type="text"
+          value={this.props.formData.email}
+          onChange={this.props.onChange.bind(null, 'email')}
+        />
+      </div>
+      <div className="row">
+        <label>User Name</label>
+        <input 
+          type="text" 
+          value={this.props.formData.username} 
+          onChange={this.props.onChange.bind(null, 'username')} 
+        />{/* bind 不仅可以绑定 this，还可以绑定第一个参数,。这里执行context设为null，因为UserDialog绑定了它的context */}
+      </div>
+      <div className="row">
+        <label>Password</label>
+        <input type="password"
+          value={this.props.formData.password}
+          onChange={this.props.onChange.bind(null, 'password')}
+        />
+      </div>
+      <div className="row actions">
+        <button type="submit">Sign Up</button>
+      </div>
+    </form>
+  )
 }
 
