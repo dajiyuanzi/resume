b4aabb435d9db5ffd46d72d4806f5587dd4adcd8
dajiyuanzi <dajiyuanzi@foxmail.com>
2020-01-03 17:12:47 +0800

    annotation

--- practice/mis30_node-server/step2/server.js
+++ practice/mis30_node-server/step2/server.js
@@ -6,12 +6,12 @@ var url = require('url')
 //req.query是get对应 的数据，req.body是post对应的数据。
 
 var routes = {//路由：不同url申请，对应不同的函数来读取url的文件路径，再写入response数据到流中
-  '/a': function(req, res){
+  '/a': function(req, res){ //路由为 /a/?a=xxx, 就返回request中的参数{'a':'xxx'}
     res.end(JSON.stringify(req.query))
   },
 
   '/b': function(req, res){
-    res.end('match /b')
+    res.end('match /b') //返回字符传'match /b'
   },
 
   '/a/c': function(req, res){
@@ -52,7 +52,7 @@ function routePath(req, res){
     })
     
   }else {
-    staticRoot(path.resolve(__dirname, 'static'), req, res)//请求的url没有找到对应的文件，就当作静态文件处理
+    staticRoot(path.resolve(__dirname, 'static'), req, res)//请求的url没有找到对应的文件，就当作静态文件处理，从static文件夹中找文件. path.resolve:方法会把一个路径或路径片段的序列解析为一个绝对路径。
   }
 }
 
