bf8a71656e7ea65c7b52ba985f22947748fdee45
dajiyuanzi <dajiyuanzi@foxmail.com>
2020-09-03 23:13:53 +0800

    fix4

---
+++ practice2/react-cnode/build/precache-manifest.fa9556edcf1cd4c80447fb5b0477ac76.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa56a3bf979a2b81020908109effb583",
+    "url": "/resume/practice2/react-cnode/build/index.html"
+  },
+  {
+    "revision": "f1840f82d1f65fe6a515",
+    "url": "/resume/practice2/react-cnode/build/static/css/main.6191e5ea.chunk.css"
+  },
+  {
+    "revision": "2db7213a2df2a78eff8d",
+    "url": "/resume/practice2/react-cnode/build/static/js/2.31baaece.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/resume/practice2/react-cnode/build/static/js/2.31baaece.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1840f82d1f65fe6a515",
+    "url": "/resume/practice2/react-cnode/build/static/js/main.7e418ffb.chunk.js"
+  },
+  {
+    "revision": "dae356168850c8af855d",
+    "url": "/resume/practice2/react-cnode/build/static/js/runtime-main.491be06c.js"
+  },
+  {
+    "revision": "86f6a7c2f6cf9844d8a9f5d088d87406",
+    "url": "/resume/practice2/react-cnode/build/static/media/cnodejs_light.86f6a7c2.svg"
+  }
+]);
